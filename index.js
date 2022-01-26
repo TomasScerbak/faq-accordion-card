a5ad96e2766de02c3fe2e3f9eb1fea8c3d216478
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-01-26 10:49:24 +0100

    arrow function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const question = document.querySelectorAll('.question');
+const arrowImage = document.getElementsByClassName('arrow');
 
 question.forEach((question) => {question.addEventListener('click', () => {
     if (question.nextElementSibling.classList.contains('is-hidden')) {
-        question.classList.add('is-bold')
-        question.nextElementSibling.classList.remove('is-hidden')
+        question.classList.add('is-bold');
+        question.nextElementSibling.classList.remove('is-hidden');
     } else {
-        question.nextElementSibling.classList.toggle('is-hidden')
+        question.nextElementSibling.classList.toggle('is-hidden');
         question.classList.remove('is-bold')
     }
+    question.lastElementChild.firstChild.classList.add('.rotate');
 })});
