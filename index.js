ed4cd2ebbb84e8e8b01ef37ba3ad544fd079fe0e
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-01-26 13:12:55 +0100

    toggle function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const question = document.querySelectorAll('.question');
+const arrowImage = document.querySelectorAll('.arrow');
 
 question.forEach((question) => {question.addEventListener('click', () => {
     if (question.nextElementSibling.classList.contains('is-hidden')) {
         question.nextElementSibling.classList.remove('is-hidden');
         question.classList.add('is-bold');
+        question.lastElementChild.classList.add('toggle-up');
     } else {
-        question.lastElementChild.firstChild.classList.remove('.rotate');
         question.nextElementSibling.classList.toggle('is-hidden');
         question.classList.remove('is-bold');
+        question.lastElementChild.classList.remove('toggle-up');
     }
 })});
